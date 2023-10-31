d28a05d985870b2ba7610491f2bf14aaafcf1aed
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2023-10-31 08:34:24 +0530

    create .env.example file to help setup environment variables

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const booksRoute = require('./routes/books');
+
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.status(200).json({request:'success'});
 });
+
+app.use('/api/books',booksRoute);
+
+//Connect to MongoDB
+const connectDB = require('./config/db');
+connectDB();
 
 const PORT = process.env.PORT;
 
