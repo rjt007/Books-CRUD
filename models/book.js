5e84c434af19351815b839b176c49a018b7f1b82
Rajat Agrawal <rajatagrawal007.ra@gmail.com>
2023-10-31 08:29:57 +0530

    create models/book.js for Book schema

---
+++ models/book.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    summary:{
+        type: String, required: true
+    }
+});
+
+module.exports = mongoose.model('Book',bookSchema);
